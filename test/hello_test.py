5ec09341b58b48f42d2589bd9a202bdb350ad51a
Aurelien Bonnel <aurelien.bonnel@symbiont.io>
2022-01-18 10:58:40 +0100

    test(lang_9): Modified test for lang9 support

--- test/hello_test.py
+++ test/hello_test.py
@@ -1,13 +1,13 @@
 import pytest
-from assembly.lang_8 import ContractRef
+from assembly.lang_9 import ContractRef
 
 @pytest.mark.usefixtures('network')
 class TestHello():
 
     @pytest.fixture
     def reset_publish(self, network):
-        network.upgrade_protocol(sympl_version=8)
-        hello = ContractRef("hello", "1.0.1", 8)
+        network.upgrade_protocol(sympl_version=9)
+        hello = ContractRef("hello", "1.0.1", 9)
         network.publish([hello])
 
     @pytest.fixture
@@ -16,7 +16,7 @@ class TestHello():
 
     @pytest.fixture
     def hello(self, network, reset_publish, key_alias):
-        return network[key_alias].hello["8-1.0.1"]
+        return network[key_alias].hello["9-1.0.1"]
 
     def test_text_returned_from_hello_world(self, hello):
         assert hello.hello_world() == "Hello Alice!"
