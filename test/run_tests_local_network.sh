397b24690bb82506cc80a6538caa4680e2b0d6d7
symbiont-tom-holder <tom.holder@symbiont.io>
2022-03-24 10:57:41 -0400

    Lang10 migration

--- test/run_tests_local_network.sh
+++ test/run_tests_local_network.sh
@@ -5,10 +5,10 @@ set -eo pipefail
 echo "--- Contracts Test Workflow"
 source ./env.sh
 
-sym local-network start --nodes 4
+sym local-network start --nodes 4 -l 10
 
 pip3 install --upgrade pip
-pip3 install symbiont-io.pytest-assembly==2.0.4.dev1
+pip3 install symbiont-io.pytest-assembly==2.0.4.dev9
 
 DEFAULT_NETWORK_CONFIG="$HOME/.symbiont/assembly-dev/dev-network/default/network-config.json"
 pytest hello_test.py --connection-file "$DEFAULT_NETWORK_CONFIG" --contract-path ../ -
